bad989ac86ce704e73bdc3b78f4e1d672726f797
Brian O'Dell <briantodell@gmail.com>
2011-12-28 11:06:12 -0500

    fixed init_designs() to work with couchdb-python

--- repoze/what/plugins/couchdb/_design/auth/views/group_users/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) { 
-    if (doc.doc_type == "User") {
-        for (var i=0; i==doc.groups.length-1; i++) {
-            emit(doc.groups[i].name, doc);
-        } 
-    }
-}
--- repoze/what/plugins/couchdb/_design/auth/views/user_by_name/map.js
+++
@@ -1,5 +0,0 @@
-function(doc) { 
-    if (doc.doc_type == "User")
-        emit(doc.username, doc);
-}
-
